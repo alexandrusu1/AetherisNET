6779c8064d885e231a4103fa4fe92637c3058991
alexandrusu1 <alexrusu795@gmail.com>
2025-12-12 16:54:29 +0200

    feat: added ethernet parsing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import time
+import struct
 
 def effect():
     print(".", end="", flush=True)
@@ -14,6 +15,15 @@ def effect():
     
     print()
 
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
+
+def ethernet_frame(data):
+
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
 def start_sniffer():
     print(r"""
     _    _____ _______ _    _ ______ _____  _____  _____ _   _ ______ _______ 
@@ -23,7 +33,7 @@ def start_sniffer():
 / ____ \| |___  | | | |  | | |____| | \ \ _| |_ ____) | |\  | |____   | |   
 /_/    \_\____| |_| |_|  |_|______|_|  \_\_____|_____/|_| \_|______|  |_|   
     
-    [*] [Linux] AetherisNET v0.1 - Network Traffic Analyzer
+    [*] [Linux] AetherisNET v0.2 - Ethernet Decoder
           
     """)
 
@@ -42,14 +52,17 @@ def start_sniffer():
     print("[*] Sniffer started successfully. Listening for packets", end="")
     effect()
 
-
     try:
         while True:
             raw_data, addr = sniffer.recvfrom(65535)
-            print(f" > Packet captured: {len(raw_data)} bytes | Interface: {addr[0]}")
+            
+
+            dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+            
+            print(f"[+] Ethernet Frame | Dest: {dest_mac} | Src: {src_mac} | Protocol: {eth_proto}")
             
     except KeyboardInterrupt:
-        print("\n[*] Stopping sniffer",end="")
+        print("\n[*] Stopping sniffer", end="")
         effect()
         sys.exit(0)
 
